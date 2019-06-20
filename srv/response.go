8a7af7d0c6e4d4a32b9648103fc4f2328315db1c
blinzki <pablo@isarq.com>
2019-06-20 02:28:57 -0300

    ajuste de datos

--- srv/response.go
+++ srv/response.go
@@ -1,4 +1,4 @@
-package response 
+package response
 
 func Response(s string) (string, string, string){
 	var s1 string = "" // msg 
@@ -18,64 +18,64 @@ func Response(s string) (string, string, string){
 		s3 = ""
 	}	
 	if s == "1" {
-		s1 = "*Torre Central* 				\n" + 
+		s1 = ""
+		s2 = "img/image1.jpeg"
+		s3 = "*Piso 1 | Torre Central* 			\n" + 
 			  "------------------- 			\n" + 
 			  "02 Puestos gerenciales		\n" + 
 			  "14 Puestos de trabajo		\n" +
 			  "02 Trabajadores de licencia		\n" +
 			  "08 Puestos disponibles		\n" 
-		s2 = "img/image1.jpeg"
-		s3 = "Piso 1 | Torre Central"
 	}
        if s == "2" {
-                s1 = "*Plaza Galicia*                           \n" +
+		s1 = ""
+		s2 = "img/image2.jpeg"
+                s3 = "*Piso 4 | Plaza Galicia*                  \n" +
                           "-------------------                  \n" +
                           "07 Puestos gerenciales               \n" +
                           "10 Puestos de trabajo                \n" +
                           "04 Trabajadores de licencia  	\n" +
                           "04 Puestos disponibles               \n"
-                s2 = "img/image2.jpeg"
-                s3 = "Piso 1 | Plaza Galicia"
         }
        if s == "3" {
-                s1 = "*Casa Matriz*    		                \n" +
+		s1 = ""
+                s2 = "img/image3.jpeg"
+                s3 = "*Piso 8 | Casa Matriz*                    \n" +
                           "-------------------                  \n" +
                           "05 Puestos gerenciales               \n" +
                           "27 Puestos de trabajo                \n" +
                           "07 Trabajadores de licencia		\n" +
                           "02 Puestos disponibles               \n"
-                s2 = "img/image3.jpeg"
-                s3 = "Piso 1 | Casa Matriz"
         }
        if s == "4" {
-                s1 = "*Corrientes 411*                          \n" +
+		s1 = ""
+                s2 = "img/image4.jpeg"
+                s3 = "*Piso 3 | Corrientes 411*                 \n" +
                           "-------------------                  \n" +
                           "03 Puestos gerenciales               \n" +
                           "14 Puestos de trabajo                \n" +
                           "00 Trabajadores de licencia  	\n" +
                           "06 Puestos disponibles               \n"
-                s2 = "img/image4.jpeg"
-                s3 = "Piso 1 | Corrientes 411"
         }
        if s == "5" {
-                s1 = "*Peron 456*				\n" +
+		s1 = ""
+                s2 = "img/image5.jpeg"
+                s3 = "*Piso 6 | Peron 456*			\n" +
                           "-------------------                  \n" +
                           "06 Puestos gerenciales               \n" +
                           "27 Puestos de trabajo                \n" +
                           "09 Trabajadores de licencia 		\n" +
                           "10 Puestos disponibles               \n"
-                s2 = "img/image5.jpeg"
-                s3 = "Piso 1 | Peron 456"
         }
        if s == "6" {
-                s1 = "*Corrientes 409*                          \n" +
+		s1 = ""
+                s2 = "img/image6.jpeg"
+                s3 = "*Piso 4 | Corrientes 409*                 \n" +
                           "-------------------                  \n" +
                           "02 Puestos gerenciales               \n" +
                           "14 Puestos de trabajo                \n" +
                           "02 Trabajadores de licencia  	\n" +
                           "08 Puestos disponibles               \n"
-                s2 = "img/image6.jpeg"
-                s3 = "Piso 1 | Corrientes 409"
         }
 	return s1, s2, s3
 }
