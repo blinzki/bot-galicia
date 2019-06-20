c643418c3728a3236ea235a0210448c5fb2c67d8
blinzki <pablo@isarq.com>
2019-06-20 01:54:35 -0300

    more items

--- srv/response.go
+++ srv/response.go
@@ -27,5 +27,55 @@ func Response(s string) (string, string, string){
 		s2 = "img/image.jpeg"
 		s3 = "Piso 1 | Torre Central"
 	}
+       if s == "2" {
+                s1 = "*Plaza Galicia*                           \n" +
+                          "-------------------                  \n" +
+                          "07 Puestos gerenciales               \n" +
+                          "10 Puestos de trabajo                \n" +
+                          "04 Trabajadores de licencia  \n" +
+                          "04 Puestos disponibles               \n"
+                s2 = "img/image.jpeg"
+                s3 = "Piso 1 | Plaza Galicia"
+        }
+       if s == "3" {
+                s1 = "*Casa Matriz*                           \n" +
+                          "-------------------                  \n" +
+                          "05 Puestos gerenciales               \n" +
+                          "27 Puestos de trabajo                \n" +
+                          "07 Trabajadores de licencia  \n" +
+                          "02 Puestos disponibles               \n"
+                s2 = "img/image.jpeg"
+                s3 = "Piso 1 | Casa Matriz"
+        }
+       if s == "4" {
+                s1 = "*Corrientes 411*                           \n" +
+                          "-------------------                  \n" +
+                          "03 Puestos gerenciales               \n" +
+                          "14 Puestos de trabajo                \n" +
+                          "00 Trabajadores de licencia  \n" +
+                          "06 Puestos disponibles               \n"
+                s2 = "img/image.jpeg"
+                s3 = "Piso 1 | Corrientes 411"
+        }
+       if s == "5" {
+                s1 = "*Peron 456*                           \n" +
+                          "-------------------                  \n" +
+                          "06 Puestos gerenciales               \n" +
+                          "27 Puestos de trabajo                \n" +
+                          "09 Trabajadores de licencia  \n" +
+                          "10 Puestos disponibles               \n"
+                s2 = "img/image.jpeg"
+                s3 = "Piso 1 | Peron 456"
+        }
+       if s == "6" {
+                s1 = "*Corrientes 409*                           \n" +
+                          "-------------------                  \n" +
+                          "02 Puestos gerenciales               \n" +
+                          "14 Puestos de trabajo                \n" +
+                          "02 Trabajadores de licencia  \n" +
+                          "08 Puestos disponibles               \n"
+                s2 = "img/image.jpeg"
+                s3 = "Piso 1 | Corrientes 409"
+        }
 	return s1, s2, s3
 }
